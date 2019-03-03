6220182e98187a8bfaece5030e671fcd844f6353
mohubetroh <45729485+mohubetroh@users.noreply.github.com>
2019-03-03 14:08:18 +0700

    Update login.js

--- login.js
+++ login.js
@@ -1,27 +1,35 @@
-$(document).ready(function(){
-	$("#login").click(function(){
-
-		var action = $("#lg-form").attr('action');
-		var form_data = {
-			username: $("#username").val(),
-			password: $("#password").val(),
-			is_ajax: 1
-		};
-
-		$.ajax({
-			type: "POST",
-			url: action,
-			data: form_data,
-			success: function(response)
-			{
-				if(response == "success")
-					$("#lg-form").slideUp('slow', function(){
-						$("#message").html('<p class="success">You have logged in successfully!</p><p>Redirecting....</p>');
-					});
-				else
-					$("#message").html('<p class="error">ERROR: Invalid username and/or password.</p>');
+function check_login()
+{
+	//Mengambil value dari input username & Password
+	var username = $('coy').val();
+	var password = $('coy').val();
+	//Ubah alamat url berikut, sesuaikan dengan alamat script pada komputer anda
+	var url_login	 = 'http://localhost/tutorial/login.php';
+	var url_admin	 = 'http://localhost/tutorial/admin.php';
+	
+	//Ubah tulisan pada button saat click login
+	$('#btnLogin').attr('value','Silahkan tunggu ...');
+	
+	//Gunakan jquery AJAX
+	$.ajax({
+		url		: url_login,
+		//mengirimkan username dan password ke script login.php
+		data	: 'var_usn='+username+'&var_pwd='+password, 
+		//Method pengiriman
+		type	: 'POST',
+		//Data yang akan diambil dari script pemroses
+		dataType: 'html',
+		//Respon jika data berhasil dikirim
+		success	: function(pesan){
+			if(pesan=='ok'){
+				//Arahkan ke halaman admin jika script pemroses mencetak kata ok
+				window.location = url_admin;
 			}
-		});
-		return false;
+			else{
+				//Cetak peringatan untuk username & password salah
+				alert(pesan);
+				$('#btnLogin').attr('value','Coba lagi ...');
+			}
+		},
 	});
-});
+}
